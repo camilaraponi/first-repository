9f273ab4b3ffa7cb1c64610d56334d2698eb0770
camilaraponi <camilaraponi@gmail.com>
2022-09-12 23:11:08 -0300

    agregue datos y nombre provisorio de la pagina

---
+++ Clase.js
@@ -0,0 +1,16 @@
+function laClaveSecreta(clave){
+    let claveOrdenada = "";
+    for (let i = 0; i < clave.lenght; i++){
+        for(let i = clave.lenght - 1; 1>= 0; i-- ){
+            claveOrdenada [i] = clave[i];
+        }
+    } return claveOrdenada;
+
+}
+
+let clave = ["i", "m", "a", "C"];
+
+///console.log(laClaveSecreta(clave));
+
+ console.log("Camila");
+
